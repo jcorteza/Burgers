b054d06b355cc0a1330d33e221c3bcd8543ac43e
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-12-20 17:38:29 -0600

    added jaws db connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,18 @@
 const mysql = require("mysql");
-module.exports = mysql.createConnection({
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection  = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: null,
     port: 3306,
     database: "burgers_db"
-});
+  });
+};
+
+connection.connect();
+module.exports = connection;
