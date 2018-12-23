38da8020fe8b34c2d6f4863e78f5cde21f9122a9
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-12-23 00:03:07 -0600

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,6 +16,7 @@ module.exports = function(app){
     });
     app.post("/", (req, res) => {
         const name = req.body.burgerName;
+        console.log(name);
         burgers.selectOne(name, function(data) {
             if(data) {
                 burgers.updateOne(0, name, (data) => {
@@ -29,6 +30,7 @@ module.exports = function(app){
         });
     });
     app.put("/", (req, res) => {
+        console.log(req.body.burgerName);
         burgers.updateOne(1, req.body.burgerName, (data) => {
             return res.json({successMessage:`Burger was successfully updated with the following data: ${JSON.stringify(data)}`});
         });
