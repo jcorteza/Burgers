07b95bb0f9a21afba8ea54c75b22b5745e0312a8
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-12-22 23:26:26 -0600

    added express.static to serve public files

--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 const express = require("express");
 let app = express();
 const PORT = process.env.PORT || 4020;
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 //setting up handlebars for application use
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgers_controller")(app);
+require("./controllers/burgers_controller")(app);
 
 app.listen(PORT, function(err, response){
     if(err) throw err;
